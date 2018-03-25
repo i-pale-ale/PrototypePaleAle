a478f62b995b53aa5108df6a9493cc79de7a4fd7
dhananjaybagi <dhananjay.bagi@gmail.com>
2018-03-25 13:11:35 -0500

    First commit of entire Boot

---
+++ src/main/java/com/paleale/boot/PrototypePaleAleBoot1Application.java
@@ -0,0 +1,12 @@
+package com.paleale.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrototypePaleAleBoot1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrototypePaleAleBoot1Application.class, args);
+	}
+}
---
+++ src/main/java/com/paleale/boot/hello/HelloController.java
@@ -0,0 +1,13 @@
+package com.paleale.boot.hello;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+	
+	@RequestMapping("/hi")
+	public @ResponseBody String hellowWorld(){
+		return "Hello World!";
+	}
+}
