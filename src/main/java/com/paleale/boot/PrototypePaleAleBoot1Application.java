1bfdc116346103fdfcdfc72c1c95fdb91bd6b961
dhananjaybagi <dhananjay.bagi@gmail.com>
2018-04-04 21:03:43 -0500

    adding maven profiles for dev and prod

--- src/main/java/com/paleale/boot/PrototypePaleAleBoot1Application.java
+++ src/main/java/com/paleale/boot/PrototypePaleAleBoot1Application.java
@@ -2,11 +2,20 @@ package com.paleale.boot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class PrototypePaleAleBoot1Application {
+public class PrototypePaleAleBoot1Application extends SpringBootServletInitializer {
 
-	public static void main(String[] args) {
-		SpringApplication.run(PrototypePaleAleBoot1Application.class, args);
-	}
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(PrototypePaleAleBoot1Application.class);
+    }
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(PrototypePaleAleBoot1Application.class, args);
+    }
+
 }
+
